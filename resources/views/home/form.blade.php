3818ff2fd7b63a75b4ba3e55eec5d24ed510d39d
dwijpr <dwijpr@gmail.com>
2016-07-02 06:24:15 +0700

    update pay input form style

--- resources/views/home/form.blade.php
+++ resources/views/home/form.blade.php
@@ -3,32 +3,48 @@
     'class' => 'form-horizontal',
     'role' => 'form',
 ]) !!}
-    <div class="form-group{{ $errors->has('item')?' has-error':'' }}">
-        {{ Form::label('item', 'Item', [
-            'class' => 'control-label col-sm-4',
-            'for' => 'item',
-        ]) }}
-        <div class="col-sm-6" id="the-basics">
-            {{ Form::text('item', null, [
-                'class' => 'form-control typeahead',
-                'id' => 'item',
-            ]) }}
-            @if($errors->has('item'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('item') }}</strong>
-                </span>
-            @endif
-        </div>
-    </div>
-    {{ Form::bsText(
-        'price'
-    ) }}
-    {{ Form::bsText(
-        'datetime', date('Y-m-d h:i:s')
-    ) }}
     <div class="form-group">
-        <div class="col-sm-6 col-sm-offset-4">
-            <button class="btn btn-primary">
+        <div class="col-md-5{{ $errors->has('item')?' has-error':'' }}">
+            <div id="the-basics">
+                {{ Form::text('item', null, [
+                    'class' => 'form-control typeahead',
+                    'placeholder' => 'Name thing to be Paid'
+                ]) }}
+                @if($errors->has('item'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('item') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+        <div class="col-md-2{{ $errors->has('price')?' has-error':'' }}">
+            <div>
+                {{ Form::text('price', null, [
+                    'class' => 'form-control text-right',
+                    'placeholder' => '1.000.000',
+                ]) }}
+                @if($errors->has('price'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('price') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+        <div class="col-md-3{{ $errors->has('datetime')?' has-error':'' }}">
+            <div>
+                {{ Form::text('datetime', date('Y-m-d h:i:s'), [
+                    'class' => 'form-control text-center',
+                    'placeholder' => 'Paid Date & Time'
+                ]) }}
+                @if($errors->has('datetime'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('datetime') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+        <div class="col-md-2">
+            <button class="btn btn-primary" style="width: 100%;">
                 {{ 'Add' }}
             </button>
         </div>
