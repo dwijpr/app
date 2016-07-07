5899f40d7741153d7cdb4db742e55bcf85842014
dwijpr <dwijpr@gmail.com>
2016-07-08 06:50:52 +0700

    apply new home style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,65 @@
 
 @section('_style')
 <style>
-body {
-    background: #f4f4f4;
-}
 .add-image-container {
-    background: #efefef;
-    padding: 32px 64px 8px;
-    border-bottom: 1px solid #777;
+    padding: 16px 2px;
+}
+.add-image-container .form-wrapper {
+    padding: 0 64px;
+}
+.add-image-container hr{
+    border-color: #f4f4f4;
 }
 .img {
-    padding: 4px;
-    height: 160px;
+    position: relative;
+    padding: 0;
+    border: 2px solid white;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
 }
+@media (min-width: 768px) {
+    .img {
+        border-width: 8px;
+    }
+    .add-image-container {
+        padding-left: 8px;
+        padding-right: 8px;
+    }
+}
+@media (min-width: 992px) {
+    .img {
+        border-width: 16px;
+    }
+    .add-image-container {
+        padding-left: 16px;
+        padding-right: 16px;
+    }
+}
+.img:before {
+    content: "";
+    display: block;
+    padding-top: 100%;
+}
+.img div {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 1px solid #f4f4f4;
+}
+
+/*
 .img table {
     width: 100%;
 }
 .img table td {
     text-align: center;
     vertical-align: middle;
-    height: 156px;
     padding: 0;
 }
 .img-item {
@@ -41,6 +81,7 @@ body {
 .img table td:hover .img-delete {
     display: inline;
 }
+*/
 </style>
 @endsection
 
--- resources/views/home/content.blade.php
+++ resources/views/home/content.blade.php
@@ -1,67 +1,81 @@
-<div class="container-fluid">
+<div class="container">
     <div class="row">
-        <div class="col-md-12 add-image-container">
-            <div>
-                {{ Form::open([
-                    'url' => '/home',
-                    'class' => 'form-horizontal',
-                    'role' => 'form',
-                    'files' => true,
-                ]) }}
-                    <div class="form-group{{
-                        $errors->has('image')?' has-error':''
-                    }}">
-                        <div class="col-sm-9">
-                            {{ Form::file('image', [
-                                'class' => 'form-control square'
-                            ]) }}
-                            @if($errors->has('image'))
-                                <span class="help-block">
-                                    <strong>
-                                        {{ $errors->first('image') }}
-                                    </strong>
-                                </span>
-                            @endif
-                        </div>
-                        <div class="col-sm-3">
-                            <button class="btn btn-primary full square">
-                                {{ 'Add' }}
-                            </button>
-                        </div>
-                    </div>
-                {{ Form::close() }}
-            </div>
-        </div>
-        <div class="col-md-12">
+        <div class="col-md-10 col-md-offset-1">
             <div class="row">
-                @foreach($objects as $o)
-                    <div class="col-md-3 col-sm-4 col-xs-6 img">
-                        <table>
-                            <tr>
-                                <td>
-                                    {{ Html::image(
-                                        '/'.$o->id.'/sm', $o->name, [
-                                            'class' => 'img-item'
-                                        ]
-                                    ) }}
-                                    {{ Form::open([
-                                        'url' => $o->id,
-                                        'method' => 'DELETE',
-                                        'class' => 'img-delete'
+                <div class="col-md-12 add-image-container">
+                    <div class="form-wrapper">
+                        {{ Form::open([
+                            'url' => '/home',
+                            'class' => 'form-horizontal',
+                            'role' => 'form',
+                            'files' => true,
+                        ]) }}
+                            <div class="form-group{{
+                                $errors->has('image')?' has-error':''
+                            }}">
+                                <div class="col-sm-9">
+                                    {{ Form::file('image', [
+                                        'class' => 'form-control square'
                                     ]) }}
-                                        <a
-                                            href="javascript:"
-                                            class="no"
-                                            onclick="confirmSubmit(this)"
-                                        >
-                                            <i class="fa fa-trash"></i>
-                                        </a>
-                                    {{ Form::close() }}
-                                </td>
-                            </tr>
-                        </table>
+                                    @if($errors->has('image'))
+                                        <span class="help-block">
+                                            <strong>
+                                                {{ $errors->first('image') }}
+                                            </strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="col-sm-3">
+                                    <button class="btn btn-primary full square">
+                                        {{ 'Add' }}
+                                    </button>
+                                </div>
+                            </div>
+                        {{ Form::close() }}
                     </div>
-                @endforeach
+                    <hr>
+                </div>
+                <div class="col-md-12">
+                    <div class="row">
+                        @foreach($objects as $o)
+                            <div
+                                class="col-xs-4 img"
+                                style="
+                                    background-image:
+                                        url({{url($o->id.'/md')}});
+                                "
+                            >
+                                <div></div>
+                                <!--
+                                <table>
+                                    <tr>
+                                        <td>
+                                            {{ Html::image(
+                                                '/'.$o->id.'/sm', $o->name, [
+                                                    'class' => 'img-item'
+                                                ]
+                                            ) }}
+                                            {{ Form::open([
+                                                'url' => $o->id,
+                                                'method' => 'DELETE',
+                                                'class' => 'img-delete'
+                                            ]) }}
+                                                <a
+                                                    href="javascript:"
+                                                    class="no"
+                                                    onclick="confirmSubmit(this)"
+                                                >
+                                                    <i class="fa fa-trash"></i>
+                                                </a>
+                                            {{ Form::close() }}
+                                        </td>
+                                    </tr>
+                                </table>
+                                -->
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
             </div>
         </div>
     </div>
