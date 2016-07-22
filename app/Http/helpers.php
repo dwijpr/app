aeb88b82d9d02d31776cae32aa2c79c6e82281c3
dwijpr <dwijpr@gmail.com>
2016-07-23 04:11:20 +0700

    remove unused

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,17 +1,5 @@
 <?php
 
-if (!function_exists('os_to_kvs')) {
-    function os_to_kvs($os, $valName, $keyName = 'id') {
-        $kvs = [];
-        foreach ($os as $key => $value) {
-            $_key = $value->{$keyName};
-            $_val = $value->{$valName};
-            $kvs[$_key] = $_val;
-        }
-        return $kvs;
-    }
-}
-
 if (!function_exists('no_ext')) {
     function no_ext($filename) {
         return preg_replace("/\.[^.]+$/", "", $filename);
